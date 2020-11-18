ed62dce4325db5149738e7b2bdc01e832be0b3cc
Sammy Wong <66samwon@gafe.molndal.se>
2020-11-18 11:18:12 +0100

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int value = 1;
+
+            //while (value <= 50)
+            //{
+            //    Console.WriteLine(value);
+            //    value++;
+            //}
+
+
+            //value = 0;
+
+            //do
+            //{
+            //    Console.WriteLine(value);
+            //    value++;
+            //}
+            //while (value <= 50);
+
+
+
+        }
+    }
+}
